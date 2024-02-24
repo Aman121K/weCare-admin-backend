acdcb316332d7426d46ce7bd65a6c014227ee394
abc <abc@abcs-MacBook-Pro.local>
2024-02-24 17:37:11 +0530

    port chnages

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connectDB = require("./db");
 const bodyParser = require("body-parser");
 const routes = require("./routes/index");
 const cors = require("cors");
-const PORT = 6000
+const PORT = 6001
 
 app.use(cors({
     origin: "*",
